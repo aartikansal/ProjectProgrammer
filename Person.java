754ba37a7b5e2e60f4ab73a1a07710ab8bd39134
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-08 21:17:12 -0500

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,2 +1,27 @@
-public interface Person {
-}
+
+    public interface Person {
+        class User implements Person {
+            void live() {
+                System.out.println("I usually just live.");
+            }
+        }
+
+        class Loser implements Person {
+            void doNothing() {
+                System.out.println("I usually do nothing.");
+            }
+        }
+
+        class Coder implements Person {
+            void writeCode() {
+                System.out.println("I usually write code.");
+            }
+        }
+
+        class Programmer implements Person {
+            void enjoy() {
+                System.out.println("It's a wonderful life!");
+            }
+        }
+    }
+
